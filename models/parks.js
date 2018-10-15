560c035372e2185d25fdb422973783300133134c
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-10-14 22:30:13 -0400

    Review form shows up only when user is logged in

--- models/parks.js
+++ models/parks.js
@@ -5,7 +5,8 @@ const parkSchema = new Schema({
     name: {type: String, required: true},
     img: String,
     description: String,
-    rating: {type: Number, min: 0, max: 5}
+    rating: {type: Number, min: 0, max: 5},
+    reviews: [String]
 });
 
 const Park = mongoose.model('Park', parkSchema);
